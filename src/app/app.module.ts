1a152a2581be7aa9f262d181910d86e1100c52f1
tooxtools <ousmaetech@gmail.com>
2022-09-09 15:50:53 +0000

    first2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DetailsPage } from './details/details.page';
+import { HomePage } from './home/home.page';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  bootstrap: [
+    AppComponent,
+
+  ],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { DetailsPage } from '../details/details.page';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+Articles : any;
 
-  constructor() {}
+  constructor (public navCtrl: NavController ){ 
+    this.Articles=[
+      {nom: 'Télévision', prix : 145, details: "De la Senegalaise"},
+      {nom: 'Ordinateur', prix : 389,  details: "De la Senegalaise"},
+      {nom: 'Ralonge', prix : 474,  details: "De la bonnnequiestcool"},
+      {nom: 'Tableau', prix : 254, details: "superclass"},
+      {nom: 'Decodeur', prix : 852, details: "bien taille sans trop"},
+      {nom: 'Ampoule', prix : 357,  details: "ma propre cretivité"}
+    ]
+  } 
+  showDetails( data : any): void {
+      this.navCtrl.navigateForward('/details, {data : data}');
+    }
+   
+  showData( data : any) : void{
+    console.log('data is', data);
+  }
 
-}
+  }
