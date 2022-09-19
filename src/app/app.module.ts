fca9ec95469a11d1af3698b104376d64a4183955
tooxtools <ousmaetech@gmail.com>
2022-09-19 11:21:53 +0000

    plugingoogleplus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database/';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
 
 import { IonicModule, IonicRouteStrategy, NavParams } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp } from 'firebase/app';
 
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+//import { GooglePlus } from '@ionic-native/google-plus';
+const firebaseConfig = {
+  apiKey: "AIzaSyAU6qVO0Us9_ofqxkuj2GEzyz2pgRLDi-I",
+  authDomain: "ioniclipson.firebaseapp.com",
+  projectId: "ioniclipson",
+  storageBucket: "ioniclipson.appspot.com",
+  messagingSenderId: "999112778830",
+  appId: "1:999112778830:web:bc663ad85d61e8796f0f1f"
+};
+const app = initializeApp(firebaseConfig);
 
 
 @NgModule({
   declarations: [
     AppComponent
-       
+
   ],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy,  }, NavParams],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+
+  ],
+  providers: [
+    AngularFireAuth,
+    GooglePlus,
+    { provide: 
+    RouteReuseStrategy, useClass: IonicRouteStrategy,
+  }, NavParams, ],
   bootstrap: [
-    AppComponent  
-    
+    AppComponent
+
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { Capability } from 'protractor';
+import '@codetrix-studio/capacitor-google-auth'
+import { Plugin } from '@capacitor/core';
+import { Plugins } from 'protractor/built/plugins';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +14,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  validationUser
+  UserInfo = null;
 
-  constructor() { }
+  constructor(private googlePlus: GooglePlus) { }
 
   ngOnInit() {
+  }
+  googleSignup() {
+    this.googlePlus.login({
+      webClientId: "999112778830-7df6sibj1ol5tpl24flpjjpe0pkjetts.apps.googleusercontent.com",
+      onoffline: true
+    })
+      .then((res: any) => {
+        console.log('Google singin done')
+        console.log(res)
+        let googleObject = {
+          name: res.displayName,
+          email: res.email
+        }
+
+      }).catch(err => {
+        console.log('error', err);
+        console.error(err);
+      })
+
   }
 
 }
